710834ef9f701488a2e2db9402b17862e8c3ab3e
Fasya Khuzaimah <38481704+fasyakhuza@users.noreply.github.com>
2022-12-14 12:11:58 +0800

    Update haddBackground.sh

--- optimization/forYuehShun/total_background/haddFiles/haddBackground.sh
+++ optimization/forYuehShun/total_background/haddFiles/haddBackground.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-inputdir="/eos/user/f/fkhuzaim/MET_Optimization/newRootFile_20221213/"
+inputdir="/eos/user/f/fkhuzaim/MET_Optimization/newRootFile_20221213"
 inputhaddname="haddName.txt"
 
-outputdir="/eos/user/f/fkhuzaim/MET_Optimization/merge_output/"
+outputdir="/eos/user/f/fkhuzaim/MET_Optimization/merge_output"
 mkdir $outputdir
 
 arrFile=()
@@ -12,7 +12,7 @@ while read -r line; do
     for file in `ls $inputdir`; do
         samplename=`echo $file | rev | cut -d '_' -f 1-3 --complement | rev`
         if [ $samplename == "$line" ]; then
-            arrFile+=("$inputdir$file") 
+            arrFile+=("$inputdir/$file") 
         fi
     done
 
@@ -22,6 +22,6 @@ while read -r line; do
     #echo $outputdir$line.root
     
     theFiles=`echo ${arrFile[@]}`
-    hadd $outputdir$line.root $theFiles
+    hadd $outputdir/$line.root $theFiles
 
 done < $inputhaddname
